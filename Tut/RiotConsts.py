07e77bc79ce49772021d02d9f67d0f2116487cb1
Eric He <eriche@nbp-122-172.nbp.ruw.rutgers.edu>
2018-10-22 10:12:50 -0400

    new consts

---
+++ Tut/RiotConsts.py
@@ -0,0 +1,9 @@
+match = "https://{region}.api.riotgames.com/lol/match/{version}/matchlists/by-account/{accountID}?api_key={api_key}"
+
+summoner = "https://{region}.api.riotgames.com/lol/summoner/{version}/summoners/by-name/{name}?api_key={api_key}"
+
+league = "https://{region}.api.riotgames.com/lol/league/{version}/positions/by-summoner/{id}?api_key={api_key}"
+
+game = "https://{region}.api.riotgames.com/lol/match/{version}/matches/{matchid}?api_key={api_key}"
+
+version = "v3"
